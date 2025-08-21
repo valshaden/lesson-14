8899bc9b9ba4491350c8e7737a79002be11d69b3
valshaden <valshaden@yandex.ru>
2025-08-21 15:23:46 +0300

    + read from db

--- p3.py
+++ p3.py
@@ -16,17 +16,21 @@ cursor = conn.cursor()
 #    INSERT INTO users (username, password) VALUES (?, ?)
 #''', ('user1', 'password1'))
 
-users = [
-    ('user9', 'password9'),
-    ('user10', 'password10'),
-    ('Галина', 'Карымова'),
-]
+#users = [
+#    ('user9', 'password9'),
+#    ('user10', 'password10'),
+#    ('Галина', 'Карымова'),
+#]
 
-cursor.executemany('''
-    INSERT INTO users (username, password) VALUES (?, ?)
-''', users)
+#cursor.executemany('''
+#    INSERT INTO users (username, password) VALUES (?, ?)
+#''', users)
 
+cursor.execute('''
+    SELECT * FROM users
+''')
+users = cursor.fetchall()
+print(*users, sep='\n')
 
-
-conn.commit()
+#conn.commit()
 
