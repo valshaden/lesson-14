cb93235ab1f230c876d9a84ee36ff864be93275e
valshaden <valshaden@yandex.ru>
2025-08-21 15:08:37 +0300

    +

--- p2.py
+++ p2.py
@@ -4,16 +4,16 @@ conn = sqlite3.connect('database.db')
 
 cursor = conn.cursor()
 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINNCREMENT,
-        username TEXT NOT NULL,
-        password TEXT NOT NULL
-    )
-''')
-
 #cursor.execute('''
-#    INSERT INTO users (username, password) VALUES (?, ?)
-#''', ('Alice', 'password'))
+#    CREATE TABLE IF NOT EXISTS users (
+#        id INTEGER PRIMARY KEY AUTOINCREMENT,
+#        username TEXT NOT NULL,
+#        password TEXT NOT NULL
+#    )
+#''')
+
+cursor.execute('''
+    INSERT INTO users (username, password) VALUES (?, ?)
+''', ('Alice', 'password'))
 
 conn.commit()
