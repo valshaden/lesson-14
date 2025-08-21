3a7e850d0755ae6dd9f3f3b05cd3709fdfe884dd
valshaden <valshaden@yandex.ru>
2025-08-21 14:46:38 +0300

    + db

--- p2.py
+++ p2.py
@@ -1,3 +1,15 @@
 import sqlite3
 
 conn = sqlite3.connect('database.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        age INTEGER
+    )
+''')
+
+conn.commit()
