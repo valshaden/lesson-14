a2098ec91584bfbea7b04422ccd266de9831c112
valshaden <valshaden@yandex.ru>
2025-08-21 15:44:00 +0300

    +

---
+++ p5.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+
+cursor = conn.cursor()
+
+#cursor.execute('''
+#    CREATE TABLE IF NOT EXISTS users (
+#        id INTEGER PRIMARY KEY AUTOINCREMENT,
+#        username TEXT NOT NULL,
+#        password TEXT NOT NULL
+#    )
+#''')
+
+#cursor.execute('''
+#    INSERT INTO users (username, password) VALUES (?, ?)
+#''', ('user1', 'password1'))
+
+#users = [
+#    ('user9', 'password9'),
+#    ('user10', 'password10'),
+#    ('Галина', 'Карымова'),
+#]
+
+#cursor.executemany('''
+#    INSERT INTO users (username, password) VALUES (?, ?)
+#''', users)
+
+cursor.execute("SELECT username FROM users WHERE id > 5")
+users = cursor.fetchall()
+print(*users, sep='\n')
+
+#conn.commit()
+
+for user in users:
+    print(user)  
---
+++ p6.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+
+cursor = conn.cursor()
+
+#cursor.execute('''
+#    CREATE TABLE IF NOT EXISTS users (
+#        id INTEGER PRIMARY KEY AUTOINCREMENT,
+#        username TEXT NOT NULL,
+#        password TEXT NOT NULL
+#    )
+#''')
+
+#cursor.execute('''
+#    INSERT INTO users (username, password) VALUES (?, ?)
+#''', ('user1', 'password1'))
+
+#users = [
+#    ('user9', 'password9'),
+#    ('user10', 'password10'),
+#    ('Галина', 'Карымова'),
+#]
+
+#cursor.executemany('''
+#    INSERT INTO users (username, password) VALUES (?, ?)
+#''', users)
+
+cursor.execute("UPDATE users SET username = 'Ирина' WHERE id = 1")
+conn.commit()
+
+#users = cursor.fetchall()
+#print(*users, sep='\n')
+
+#conn.commit()
+
+#for user in users:
+#    print(user)  
